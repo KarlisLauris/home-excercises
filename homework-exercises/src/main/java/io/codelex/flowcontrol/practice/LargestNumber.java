e54911f64c16b3747d0e363698a81cb61cecf2ad
KarlisLauris <karlislauris@gmail.com>
2022-09-30 18:38:29 +0300

    Labojumi

--- homework-exercises/src/main/java/io/codelex/flowcontrol/practice/LargestNumber.java
+++ homework-exercises/src/main/java/io/codelex/flowcontrol/practice/LargestNumber.java
@@ -18,10 +18,15 @@ public class LargestNumber {
         int num3 = in.nextInt();
 
         if (num1 > num2 && num1 > num3) {
-                System.out.println("The greatest: " + num1);
-            } else {
-                System.out.println("The greatest: " + num3);
-            }
+            System.out.println("The largest number is " + num1);
+        } else if (num2 > num1 && num2 > num3) {
+            System.out.println("The largest number is " + num2);
+        } else if (num3 > num1 && num3 > num2) {
+            System.out.println("The largest number is " + num3);
+        } else {
+            System.out.println("The numbers are equal");
+        }
+
     }
 
 }
