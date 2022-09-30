2571a1e936feb2136ccb6e2e89b670f015414112
KarlisLauris <karlislauris@gmail.com>
2022-09-30 18:38:29 +0300

    Labojumi

--- homework-exercises/src/main/java/io/codelex/loops/Exercise5.java
+++ homework-exercises/src/main/java/io/codelex/loops/Exercise5.java
@@ -11,14 +11,13 @@ public class Exercise5 {
         String word2 = sc.nextLine();
 
         //System.out.println(word1 + ".".repeat(30-word1.length()-word2.length()) + word2);
-        for(int i=1; i<=30; i++){
-            if(i<=word1.length()){
-                System.out.print(word1.charAt(i-1));
-            }else if(i<=30-word2.length()){
-                System.out.print(".");
-            }else{
-                System.out.print(word2.charAt(i-30+word2.length()-1));
-            }
+        int lineLength = 30;
+        StringBuilder line = new StringBuilder(word1);
+        for(int i=0; i<lineLength-word2.length()-word1.length(); i++) {
+            line.append(".");
         }
+        line.append(word2);
+        System.out.println(line);
+        System.out.println(line.length());
     }
 }
